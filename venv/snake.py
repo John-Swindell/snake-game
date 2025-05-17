58009dd335fde9e9d6506d41993454ccac9e5d7b
John-Swindell <102964572+John-Swindell@users.noreply.github.com>
2025-05-16 20:19:26 -0500

    Prevent snake from moving on top of self. Changed default directions in turtle lib to constants.

--- venv/snake.py
+++ venv/snake.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
 
+# tuples passed to goto turtle function on spawn.
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+MOVE_DISTANCE = 20
+# turtle graphics default directions.
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.__body = []
         self.build_snake()
+        self.__head = self.__body[0]
 
     def build_snake(self):
         for position in STARTING_POSITIONS:
@@ -19,18 +28,25 @@ class Snake:
     def move(self):
         for seg_num in range(len(self.__body) - 1, 0, -1):
             front_segment_pos = self.__body[seg_num - 1].pos()
+            # previous segment goes to next segment position
             self.__body[seg_num].goto(front_segment_pos)
 
-        self.__body[0].forward(20)
+        self.__head.forward(MOVE_DISTANCE)
 
+
+    # each method sets heading (direction) if the snake is not currently moving in the opposite direction.
     def up(self):
-        self.__body[0].setheading(90)
+        if self.__head.heading() != DOWN:
+            self.__head.setheading(UP)
 
     def down(self):
-        self.__body[0].setheading(270)
+        if self.__head.heading() != UP:
+            self.__head.setheading(DOWN)
 
     def left(self):
-        self.__body[0].setheading(180)
+        if self.__head.heading() != RIGHT:
+            self.__head.setheading(LEFT)
 
     def right(self):
-        self.__body[0].setheading(0)
+        if self.__head.heading() != LEFT:
+            self.__head.setheading(RIGHT)
