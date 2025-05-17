87f016e6056d9658b5eac1ed1bc7bf2a55ce5ae1
John-Swindell <102964572+John-Swindell@users.noreply.github.com>
2025-05-16 19:59:14 -0500

    Changed title

--- venv/main.py
+++ venv/main.py
@@ -5,7 +5,7 @@ from snake import Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("Slippy The Snake")
 screen.tracer(0)
 
 slippy = Snake()
